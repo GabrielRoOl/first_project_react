6e342fcb56ea0efe1a0b4b2407a204185c53b773
GabrielRoOl <144238400+GabrielRoOl@users.noreply.github.com>
2025-07-30 17:46:32 -0300

    feat: add consuming external API

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,10 +3,20 @@ import Rodape from "components/Rodape";
 import Banner from "components/Banner"
 import Titulo from "components/Titulo";
 import Card from "components/Card";
-import videos from "json/db.json";
 import styles from './Inicio.module.css'
+import { useEffect, useState } from "react";
 
 function Inicio() {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/GabrielRoOl/cinetag-api/videos')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, [])
+
     return (
         <>
             <Cabecalho />
